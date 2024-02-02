40bbf5230c33885da4b18321bbf9f68748afef3a
milenapupillo <milenapupillo@gmail.com>
2024-02-02 16:22:09 -0500

    Added events page

--- heythere/main_app/urls.py
+++ heythere/main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('events/', views.events, name='events'),
+    path('employees/', views.employees_index, name='index'),
 
 ]
--- heythere/main_app/views.py
+++ heythere/main_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
-# Define the home view
+# Home view
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+# Events view
+def events(request):
+  return render(request, 'events.html')
+
+# Employees index view
+def employees_index(request):
+  return render(request, 'employees/index.html', {
+    'employees': employees
+  })
