c37e616c4aab425b1d8addf735fd45e0663f6fd3
milenapupillo <milenapupillo@gmail.com>
2024-02-07 17:15:08 -0500

    Hamburger menu functional

--- heythere/main_app/static/js/main.js
+++ heythere/main_app/static/js/main.js
@@ -1,8 +1,15 @@
+// const elemsBtns = document.querySelectorAll(".fixed-action-btn");
+// const floatingBtn = M.FloatingActionButton.init(elemsBtns, {
+//   direction: "left",
+//   hoverEnabled: false
+// });
+
 const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
-const instanceDropdown = M.Dropdown.init(elemsDropdown, {
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
   coverTrigger: false
 });
+
 elemsSidenav = document.querySelectorAll(".sidenav");
-const instanceSidenav = M.Sidenav.init(elemsSidenav, {
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
   edge: "left"
 });
