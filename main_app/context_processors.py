d6bd21e0187c884e697b3200258f08643597c44f
jaeahn2010 <jaeahn2010@gmail.com>
2024-02-11 16:36:34 -0800

    refactored pto requests to be only edited/deleted by superuser

--- main_app/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Employee
-def show_user(request):
-    if request.user.is_superuser:
-        current_user = 'superuser'
-    elif not request.user.is_anonymous:
-        current_user = Employee.objects.get(employee_email=request.user.email)
-    else:
-        current_user = 'guest'
-    return {'current_user': current_user}
